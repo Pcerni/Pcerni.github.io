3cbaec4fab9f50f4fc6c150b75d0b552a326a22a
Pame <cnpamela85@gmail.com>
2020-11-23 11:41:51 -0400

    Actualización
    Actualización hecha para trabajo de desarrollo web (con requerimientos del trabajo)

---
+++ js/alertas.js
@@ -0,0 +1,7 @@
+var nombre;
+nombre = prompt("Bienvenido/a a ShadoWolves. Por favor, ingresa tu nombre", 'Ingresa tu nombre');
+
+var mensaje = "Bienvenido/a a la hermandad. Que tengas una linda experiencia,  ";
+mensaje = mensaje.concat(nombre);
+
+alert(mensaje);
